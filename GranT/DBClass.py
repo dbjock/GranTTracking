367131dd6f5792c8341d924a58648b6905ea029b
John <50465074+dbjock@users.noreply.github.com>
2019-07-02 22:21:09 -0500

    Added write manufactures to db and unittesting.

--- GranT/DBClass.py
+++ GranT/DBClass.py
@@ -31,3 +31,47 @@ class GtDB(object):
             logging.debug(f'none. Getting all manufactures. SQL: {sql}')
             self.c.execute(sql)
             return self.c.fetchall()
+
+    def write_manufacture(self,recid, title):
+        """
+        Writes/Creates a manufacture record.
+        All PARMS requried.
+        recid : 0 = Attempt to Create Record
+                !0 Record will update/create as needed
+        title : title/name of the manufacture (Must not be None,null,blank)
+        Returns True if successful, False if error
+        """
+        logging.debug(f"PARMS: recid: {recid}, title: {title}")
+        if not isinstance(recid, int):
+            logging.error(f"recid is not an integer. No Add/Update")
+            return False
+
+        # Can't allow blank data in (Note.. db doesn't consider this null)
+        if title is None or title == '':
+            logging.error(f"title must contain a value. No Add/Update")
+            return False
+
+        #Setting up SQL for Create or Update
+        if recid == 0: #Add record. If title uniquie, will add with unique recid
+            logging.debug(f"Creating record for title: {title}")
+            theRecord = (title,)
+            sql = "INSERT INTO manufactures (title) Values (?)"
+        else: #Add/replace record with provided data based on recID
+            logging.debug(f"Create/Update record")
+            theRecord = (recid, title)
+            sql = "INSERT OR REPLACE INTO manufactures (id, title) Values (?, ?)"
+
+        # Execute SQL
+        logging.debug(f"Sql: {sql}")
+        with self.conn:
+            try:
+                self.c.execute(sql, theRecord)
+            except sqlite3.IntegrityError as e:
+                logging.warning(f"theRecord: {theRecord} sqlite integrity error: {e.args[0]}")
+                return False
+            except:
+                logging.error('Something went wrong', exc_info = True)
+                return False
+
+        logging.info(f"theRecord: {theRecord} : committed")
+        return True
--- tests/test_manufacture.py
+++ tests/test_manufacture.py
@@ -25,26 +25,52 @@ class TestMfgClass(unittest.TestCase):
 class TestMfgDatabase(unittest.TestCase):
     myDb = DBClass.GtDB(gtcfg.dbcfg['dbFile'])
 
-    def test_known(self):
+    def test_Getknown(self):
         theReturn = self.myDb.get_manufacture(1)
         self.assertGreater(len(theReturn),1,"One record with more than 1 element should have been returned")
 
-    def test_NoRec(self):
+    def test_GetNoRec(self):
         theReturn = self.myDb.get_manufacture(99999)
         self.assertIsNone(theReturn,"Expected no record to be returned")
 
-    def test_noInt(self):
+    def test_GetnoInt(self):
         theReturn = self.myDb.get_manufacture("1")
         self.assertFalse(theReturn,"Expected false. Non integer was passed")
 
-    def test_AllRecs(self):
+    def test_GetAllRecs(self):
         theReturn = self.myDb.get_manufacture(0)
         self.assertGreater(len(theReturn),3,"More than 3 elements should be return with all records")
+
+    def test_CreateRec0(self):
+        theReturn = self.myDb.write_manufacture(0,"Adding a New Record")
+        self.assertTrue(theReturn,"Should have been able to create a new record")
+
+    def test_CreateRec999(self):
+        theReturn = self.myDb.write_manufacture(999,"Rec 999 added")
+        self.assertTrue(theReturn,"Should have been able to create a new record 999")
+
+    def test_CreateBlankName(self):
+        theReturn = self.myDb.write_manufacture(0,"")
+        self.assertFalse(theReturn,"Blank name should not be allowed")
+
+    def test_CreateNoneName(self):
+        theReturn = self.myDb.write_manufacture(0,None)
+        self.assertFalse(theReturn,"Blank name should not be allowed")
+
+    def test_CreateNonInt(self):
+        theReturn = self.myDb.write_manufacture("A",'Testing Non number recID')
+        self.assertFalse(theReturn,"Non INT recid should not be allowed")
+
+    def test_UpdateRec(self):
+        #TODO need to add query to ensure correct name comes back with recid
+        theReturn = self.myDb.write_manufacture(2,"Updating Record")
+        self.assertTrue(theReturn,"Record should have been updated")
 
 if __name__ == '__main__':
     unittest.main()
 
 logging.config.fileConfig('logging.conf', defaults=None, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
-logging.info("TEsting stuff loaded")
+logging.info("==========================")
+logging.info(f"Testing Database: {gtcfg.dbcfg['dbFile']}")
 
