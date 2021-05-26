f42fca3cf32de66e987c028a49169ff010452dfc
dbjock (john) <junkyardjojo@hotmail.com>
2021-05-26 16:47:49 -0500

    Added logging section to app configuration

--- GranT/gtcfg.py
+++ GranT/gtcfg.py
@@ -3,14 +3,20 @@ import configparser
 import os
 
 logger = logging.getLogger(__name__)
+
+
 def load_config(cfgFile):
     """
     Load configuraton file and store in sectional dict\n
     cfgFile : Configuration file to open (requried)\n
     """
     config.read(cfgFile)
-    dbcfg['dbFile'] = config.get('database','dbfile',fallback=None)
+    dbcfg['dbFile'] = config.get('database', 'dbfile', fallback=None)
+    logcfg['logDir'] = config.get('logging', 'logDir', fallback=None)
+    logcfg['level'] = config.get('logging', 'level', fallback="INFO")
+
 
 dbcfg = dict()
+logcfg = dict()
 config = configparser.ConfigParser()
 load_config('./GTTracking.conf')
