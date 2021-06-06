d80ca5eca2228fa990a2f582b31a2f58d933bdb1
dbjock (john) <junkyardjojo@hotmail.com>
2021-06-06 15:21:31 -0500

    cli updates to catch up.

--- cli.py
+++ cli.py
@@ -120,7 +120,7 @@ def main():
     while True:
         try:
             userCmd = session.prompt(
-                'test-GranTT > ', completer=completer, complete_while_typing=True)
+                '  GranTT > ', completer=completer, complete_while_typing=True)
         except KeyboardInterrupt:
             log.info("Keyboard Interrupt. Exiting Application")
             break
@@ -143,6 +143,8 @@ def main():
                 else:
                     log.info("add command not complete")
                     print("Please provde an object for the add command.")
+            elif action == 'cls' or action == 'clear':
+                cls()
             elif action == 'list':
                 if userCmd.find(' ') != -1:
                     objCmd = userCmd[userCmd.find(' '):]
@@ -183,47 +185,12 @@ def addAction(cmd):
     log.debug(f"obj={obj}")
     if obj == "collection":
         addCollectionCmd(cmd[len(obj):].strip())
+    elif obj == "race":
+        addRaceCmd(cmd[len(obj):].strip())
     else:  # Unknown object
         print_formatted_text(
             HTML(f'<ansired>ERROR</ansired> - Unknown <ansigreen>add</ansigreen> object <b>{obj}</b>'))
         log.info("Unknown object for add action")
-
-
-def addCollectionCmd(args):
-    """What to do when asked to add a collection"""
-    log.debug(f"args passed: {args}")
-    log.debug(f"length of args: {len(args)}")
-    if len(args) > 0 and args.find("=") > 0:  # Have valid args
-        if args.split('=')[0].strip() == 'leagueId':
-            id = args.split('=')[1].strip()
-        else:  # Unknown args passed
-            log.info(
-                f"Unknown add collection argument {args.split('=')[0].strip()}")
-            print_formatted_text(
-                HTML(f"<ansired>ERROR</ansired> - Unknown add collection argument <b>{args.split('=')[0].strip()}</b>."))
-            return
-    else:  # No args passed
-        log.info("No args passed. Getting user to select a league")
-        id = pickLeague()
-        log.info(f"User picked league id {id}")
-        if id == None:  # No League picked
-            log.info("No league select. Cancel add collection")
-            print("Canceled add collection")
-            return
-        log.info(
-            f"Get user input for adding a race collection to league id = {id}")
-
-    r = addCollection(id)
-    if r[0] != 0:  # Save was not successful
-        x = f'  <ansiyellow>Unable to add Race Collection. Return Code: {r[0]} Desc: {r[1]}</ansiyellow>'
-        print_formatted_text(HTML(x))
-        log.info(
-            f"Unable to add Race Collection. Return Code: {r[0]} Desc: {r[1]}")
-        return
-    else:  # Race collection added
-        x = f'  <ansigreen>Race Collection Added</ansigreen>'
-        print_formatted_text(HTML(x))
-        log.info(f"Race Collection Added")
 
 
 def addCollection(leagueId):
@@ -274,6 +241,90 @@ def addCollection(leagueId):
     r = GTDBConn1.addRaceCollection(rCollection)
     log.debug(f'result from adding racecollection: {r}')
     return r
+
+
+def addCollectionCmd(args):
+    """What to do when asked to add a collection"""
+    log.debug(f"args passed: {args}")
+    log.debug(f"length of args: {len(args)}")
+    if len(args) > 0 and args.find("=") > 0:  # Have valid args
+        if args.split('=')[0].strip() == 'leagueId':
+            id = args.split('=')[1].strip()
+        else:  # Unknown args passed
+            log.info(
+                f"Unknown add collection argument {args.split('=')[0].strip()}")
+            print_formatted_text(
+                HTML(f"<ansired>ERROR</ansired> - Unknown add collection argument <b>{args.split('=')[0].strip()}</b>."))
+            return
+    else:  # No args passed
+        log.info("No args passed. Getting user to select a league")
+        id = pickLeague(text="Add a race collection to which league?")
+        log.info(f"User picked league id {id}")
+        if id == None:  # No League picked
+            log.info("No league select. Cancel add collection")
+            print("Canceled add collection")
+            return
+        log.info(
+            f"Get user input for adding a race collection to league id = {id}")
+
+    r = addCollection(id)
+    if r[0] != 0:  # Save was not successful
+        x = f'  <ansiyellow>Unable to add Race Collection. Return Code: {r[0]} Desc: {r[1]}</ansiyellow>'
+        print_formatted_text(HTML(x))
+        log.info(
+            f"Unable to add Race Collection. Return Code: {r[0]} Desc: {r[1]}")
+        return
+    else:  # Race collection added
+        x = f'  <ansigreen>Race Collection Added</ansigreen>'
+        print_formatted_text(HTML(x))
+        log.info(f"Race Collection Added")
+
+
+def addRaceCmd(args):
+    log.debug(f"args passed: {args}")
+    log.debug(f"length of args: {len(args)}")
+    # At this time not going to parse any args. Just ignoring
+    cls()
+    # Prompt user for Track
+    x = pickTrack(text="Which track for the new race?")
+    if x == None:  # no track was selected
+        log.info("No track selected")
+        print(
+            f"No track was selected for new race")
+        return
+    track = GTDBConn1.getTrack(value=x)
+
+    # Prompt user for track layout
+    x = pickTrackLayout(track.id, track.name,
+                        text=f'Which layout for the new race?')
+    if x == None:  # no layout was selected
+        log.info("No layout selected")
+        print(f"No layout selected for new race on {track.name} track")
+        return
+    tLayout = GTDBConn1.getLayout(x)
+
+    x = pickLeague(text="Which League for the new race?")
+    if x == None:  # no league picked
+        log.info("No league selected.")
+        print("No league selected for new race")
+        return
+    log.info(f"Loading league object for leagueid={x}")
+    league = GTDBConn1.getLeague(value=x)
+
+    # Prompt user for leagues race collection
+    x = pickRaceCollection(
+        league.id, league.name, text=f"Which race collection for the new race?")
+    if x == None:  # no race collection selected
+        log.info(
+            f"No race collection was selected for league ({league.id}) {league.name}")
+        print("No race collection selected")
+        return
+    rcCollection = GTDBConn1.getRaceCollection(x)
+
+    x = f"Track Layout   : {tLayout.track.name} - {tLayout.name} -- layout Id: {tLayout.id}"
+    print(x)
+    x = f"Race Collection: {rcCollection.league.name} - {rcCollection.name} -- collection Id: {rcCollection.id}"
+    print(x)
 
 
 def displayCarCats(theList):
@@ -337,7 +388,7 @@ def displayTrack(trackObj):
         cText = f"      {str('No country assigned to this track')[0:50].ljust(50)}"
         region = "N/A"
     else:  # Country info
-        cText = f"({trackObj.country.id}) {trackObj.country.cntryName[0:50].ljust(50)}"
+        cText = f"{trackObj.country.cntryName[0:50].ljust(50)}"
         region = trackObj.country.region
 
     x = f"<b><u>Country:</u></b> {cText} <b>Region:</b> {region}"
@@ -346,22 +397,21 @@ def displayTrack(trackObj):
     # Get track layout List
     tLayoutList = GTDBConn1.getLayoutList(trackObj.id)
     print(f"Layouts:")
-    # Layout id column (tlID): width 2, justification right
-    # Layout name column (tlName): width 30, justification left
     tlID = "ID"
     tlName = "Name".ljust(30)
-    rowStr = f" {tlID} | {tlName}"
+    tlMiles = "Miles"
+    rowStr = f" {tlID} | {tlName} | {tlMiles} |"
     print(rowStr)
-    print("-" * 78)  # header seperator
+    print("-" * 80)  # header seperator
     # display track layouts
     for r in tLayoutList:
-        # print(r)
         tlID = str(r[0])[0:2].rjust(2)
         tlName = r[1][0:30].ljust(30)
-        rowStr = f" {tlID} | {tlName} |"
+        tlMiles = "{:.2f}".format(r[2]).rjust(5)
+        rowStr = f" {tlID} | {tlName} | {tlMiles} |"
         print(rowStr)
 
-    print("=" * 78)
+    print("=" * 80)
 
 
 def help(arg):
@@ -393,6 +443,7 @@ def listAction(cmd):
         cmd([string]): The object and its args to list
     """
     cmd = cmd.strip()
+    cls()
     # TODO: rename listObj -> obj (like what was done in addAction)
     listObj = cmd.split()[0]
     log.debug(f"listObj={listObj}")
@@ -491,7 +542,7 @@ def listRaceCollections(args):
     displayCollections(theList, league)
 
 
-def pickLeague():
+def pickLeague(text='Select a League'):
     """Dialog box for user to select a League
 
     Returns:
@@ -502,14 +553,25 @@ def pickLeague():
     log.info("Displaying leagues for user to choose")
     result = radiolist_dialog(
         title="Leagues",
-        text="Select a League",
+        text=text,
         values=pickList
     ).run()
-    log.info(f"User choose: {result}")
+    log.info(f"User choose leagueId: {result}")
     return result
 
 
-def pickTrack():
+def pickRaceCollection(leagueId, lName, text='Select one'):
+    log.info(f"Getting race collections for ({leagueId}) {lName} league")
+    picklist = GTDBConn1.getRaceCollectionList(leagueId)
+    log.info(f"Displaying collections for user to choose")
+    result = radiolist_dialog(title=f"Race Collections for {lName}",
+                              text=text,
+                              values=picklist).run()
+    log.info(f"User choose collection id: {result}")
+    return result
+
+
+def pickTrack(text="Select a Track"):
     """Dialog box for user to select a a track
     Returns: the trackID user picked
     """
@@ -518,14 +580,14 @@ def pickTrack():
     log.info("display tracks for user to choose")
     result = radiolist_dialog(
         title="Tracks",
-        text="Select a Track",
+        text=text,
         values=pickList
     ).run()
     log.info(f"User choose: {result}")
     return result
 
 
-def pickTrackLayout(trackID, trackName):
+def pickTrackLayout(trackID, trackName, text='Select one'):
     """Dialog box for user to select a track layout from a track
 
     Args:
@@ -539,8 +601,8 @@ def pickTrackLayout(trackID, trackName):
     log.info("getting track layouts for trackID:{trackID}")
     pickList = GTDBConn1.getLayoutList(trackID)
     log.info("Display track layouts for user to choose")
-    result = radiolist_dialog(title="Track Layouts",
-                              text=f"Select a layout for track {trackName}",
+    result = radiolist_dialog(title=f"Track Layouts for track {trackName}",
+                              text=text,
                               values=pickList).run()
     log.info(f"User choose: {result}")
     return result
